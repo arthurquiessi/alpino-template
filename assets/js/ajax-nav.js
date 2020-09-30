41f6293210c0067a6a19bf482e5d28b1a91ef6af
arthurquiessi <arthur.bianchi.quiessi@gmail.com>
2020-09-30 16:22:43 -0300

    new

---
+++ assets/js/ajax-nav.js
@@ -0,0 +1,19 @@
+function navegationAjax(url, seletor) {
+    if (!url || !seletor) return
+    const elemento = document.querySelector(seletor)
+    fetch(url)
+        .then(resp => resp.text())
+        .then(html => {
+            elemento.innerHTML = html
+        })
+}
+
+document.querySelectorAll('[wm-link]').forEach(link => {
+    const url = link.attributes['wm-link'].value
+    const seletorDestino = link.attributes['wm-destino'].value
+
+    link.onclick = e => {
+        e.preventDefault()
+        navegationAjax(url, seletorDestino)
+    }
+})
